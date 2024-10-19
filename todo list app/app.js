b42506f627902d896a135f80128d7b09890a543e
CoolBeachBall <coolbeachball@proton.me>
2024-10-19 20:19:16 +0300

    UI update

---
+++ todo list app/app.js
@@ -0,0 +1,8 @@
+const pinButton = document.getElementsByClassName('pin-button');
+const inputDisplay = document.getElementsByClassName('input-display');
+const listInput = document.getElementsByClassName('list-input');
+const list = document.querySelectorAll("li");
+
+function pinTheList() {
+     
+}
